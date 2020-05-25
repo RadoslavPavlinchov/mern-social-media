162dff3e2431380038bcd2300f12d7febc08ea9a
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-05-25 20:32:45 +0300

    Created an express config file and replaced the default server

--- server/server.js
+++ server/server.js
@@ -1,32 +1,9 @@
-import path from 'path';
-import express from 'express';
-import { MongoClient } from 'mongodb';
-import template from './../template';
-//  Comment out!!! Should be used only for development only, because the React code will compile and bundle in dist.
-import devBundle from './devBundle';
+import config from '../config/config';
+import app from './express';
 
-const app = express();
-//  Comment out!!! Should be used only for development only, because the React code will compile and bundle in dist
-devBundle.compile(app);
-
-const CURRENT_WORKING_DIR = process.cwd();
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
-
-app.get('/', (req, res) => {
-     res.status(200).send(template());
-});
-
-let port = process.env.PORT || 3000;
-app.listen(port, function onStart(err) {
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
   }
-  console.info('Server started on port %s.', port);
+  console.log(`Server started on port ${config.port}`);
 });
-
-// Database connection
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSocialMedia';
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db)=>{
-  console.log('Connected successfully to mongodb server');
-  db.close();
-})
